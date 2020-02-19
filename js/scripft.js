3ed7a5e225e332a79e0dda04b7215de61bbf8837
Luis Humberto <is707485@iteso.mx>
2020-02-19 09:27:05 -0600

    cambio del 19 y evento click añadido

--- js/scripft.js
+++ js/scripft.js
@@ -1,8 +1,10 @@
 //Obtener elemento principles de el html
 var conTainerCell = document.getElementById("container-cell");
 var conTainerPiece = document.getElementById("container-piece");
+var selectedPiece= null;
 
 createBoard();
+createPieces();
 
 
 function createCell(width, height){
@@ -12,7 +14,27 @@ function createCell(width, height){
 	 cellElement.style.height = height;
 	 cellElement.style.border="1px solid black";
 	 cellElement.style.backgroundColor = "#124242";
+	 cellElement.onclick=clickCell;
 	 return cellElement;
+}
+function createPiece(width, height, piece){
+	var cellElement = document.createElement("div");
+	var pieceElement = document.createElement("img");
+	
+	//configurando la celda para la pieza dentro del contenedor de piezas
+	cellElement.style.width=width;
+	cellElement.style.height=height;
+	
+	// Configurando la piza dentro del contenedor pieza
+	pieceElement.style.width=width;
+	pieceElement.style.height=height;
+	pieceElement.style.border="1px solid black";
+	pieceElement.src = piece.image;
+	pieceElement.onclick= clickPiece;
+	
+	cellElement.appendChild(pieceElement);
+	
+	return cellElement;
 }
 function createBoard(){
 	var width = conTainerCell.offsetWidth;
@@ -26,4 +48,47 @@ function createBoard(){
 }
 function addCell(element){
 	conTainerCell.appendChild(element);
+}
+function generatePieceData(){
+	//generamos una lista de piesas
+	var pieces = [];
+	let c=0;
+	for(let i=0;i<4;i++){
+		for(let q=0;q<4;q++){
+			let piece= {
+			image: "img/m"+(i+1)+"-"+(q+1)+".jpg",position: c//acomodalo para lo de los nombres
+			}
+			c++;
+			pieces.push(piece);
+			
+		}
+	}
+	return pieces; 	
+}
+function createPieces(){
+	var width = conTainerPiece.offsetWidth;
+	var height = conTainerPiece.offsetHeight;
+	width /=4;
+	height /=4;
+	var pieces = generatePieceData();
+	for(let i=0;i<16;i++){
+		let pieceElement=createPiece(width,height,pieces[i]);
+		addPiece(pieceElement);
+	}
+}
+function addPiece(element){
+	conTainerPiece.appendChild(element);
+	
+}
+function clickPiece(e){
+	var piece = e.target;
+	selectedPiece=piece;
+}
+function clickCell(e){
+	if(selectedPiece){
+		let cell = e.target;
+		cell.appendChild(selectedPiece);
+	}else{
+		console.log("Seleciona una piesa");
+	}
 }
